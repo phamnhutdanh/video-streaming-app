5891806d1ad0b433d062bc04f5dd50c16b769fac
ACER <huyhoang432002@gmail.com>
2023-12-08 09:36:02 +0700

    follow, like, dislike, save button

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,15 +1,21 @@
-import { videoRouter } from "~/server/api/routers/video";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { videoRouter } from "./routers/video";
+import { userRouter } from "./routers/user";
+import { commentRouter } from "./routers/comment";
 import { videoEngagementRouter } from "./routers/videoEngagement";
-
+import { playlistRouter } from "./routers/playlist";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  user: userRouter,
   video: videoRouter,
+  comment: commentRouter,
   videoEngagement: videoEngagementRouter,
+  playlist: playlistRouter,
+ 
 });
 
 // export type definition of API
